97b2eaa26e2245236bbb024846216079d50037e4
MladenPetric <mpetric2004@gmail.com>
2024-11-12 21:43:27 +0100

    [ADD] SplashActivity(changeScreen)

--- EventIO/app/src/main/java/com/example/eventio/SplashActivity.java
+++ EventIO/app/src/main/java/com/example/eventio/SplashActivity.java
@@ -2,23 +2,43 @@ package com.example.eventio;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        changeScreen();
+
+    }
+    private void changeScreen() {
+        //Prebacivanje na LoginActivity nakon 5 sekundi
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(task,5000);
+
     }
 }
